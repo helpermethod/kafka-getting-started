ac37d618c7b4a860e8be77445304dea64d0d2ffa
oweiler <weiler@predic8.de>
2017-10-05 16:53:30 +0200

    Update Java sample

--- samples/java/src/main/java/com/github/helpermethod/Consumer.java
+++ samples/java/src/main/java/com/github/helpermethod/Consumer.java
@@ -3,30 +3,44 @@ package com.github.helpermethod;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.serialization.IntegerDeserializer;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Properties;
 
 import static java.util.Collections.singletonList;
-import static org.apache.kafka.clients.consumer.ConsumerConfig.*;
+import static org.apache.kafka.clients.consumer.ConsumerConfig.AUTO_OFFSET_RESET_CONFIG;
+import static org.apache.kafka.clients.consumer.ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG;
+import static org.apache.kafka.clients.consumer.ConsumerConfig.GROUP_ID_CONFIG;
+import static org.apache.kafka.clients.consumer.ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG;
+import static org.apache.kafka.clients.consumer.ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG;
 
 public class Consumer {
-    private static Properties properties() {
-        Properties properties = new Properties();
+	private static final Logger log = LoggerFactory.getLogger(Consumer.class);
 
-        properties.setProperty(BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        properties.setProperty(KEY_DESERIALIZER_CLASS_CONFIG, IntegerDeserializer.class.getName());
-        properties.setProperty(VALUE_DESERIALIZER_CLASS_CONFIG, IntegerDeserializer.class.getName());
-        properties.setProperty(GROUP_ID_CONFIG, "test");
+	private static Properties properties() {
+		Properties properties = new Properties();
 
-        return properties;
-    }
+		properties.setProperty(BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+		properties.setProperty(KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+		properties.setProperty(VALUE_DESERIALIZER_CLASS_CONFIG, IntegerDeserializer.class.getName());
+		properties.setProperty(GROUP_ID_CONFIG, "test");
 
-    public static void main(String[] args) {
-        KafkaConsumer<Integer, Integer> kafkaConsumer = new KafkaConsumer<>(properties());
-        kafkaConsumer.subscribe(singletonList("test"));
+		return properties;
+	}
 
-        while (true) {
-            kafkaConsumer.poll(100).forEach(r -> System.out.printf("offset: %d, key: %d, value: %d\n", r.offset(), r.key(), r.value()));
-        }
-    }
+	public static void main(String[] args) {
+		log.info("Starting consumer...");
+
+		try (KafkaConsumer<Integer, Integer> kafkaConsumer = new KafkaConsumer<>(properties())) {
+			kafkaConsumer.subscribe(singletonList("workshop"));
+
+			kafkaConsumer.poll(0);
+			kafkaConsumer.seekToBeginning(kafkaConsumer.assignment());
+
+			while (true) {
+				kafkaConsumer.poll(100).forEach(r -> log.info("offset: {}, key: {}, value: {}", r.offset(), r.key(), r.value()));
+			}
+		}
+	}
 }
--- samples/java/src/main/java/com/github/helpermethod/Producer.java
+++ samples/java/src/main/java/com/github/helpermethod/Producer.java
@@ -1,34 +1,46 @@
 package com.github.helpermethod;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.IntegerSerializer;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Properties;
+import java.util.Random;
 
-import static java.util.stream.IntStream.range;
-import static org.apache.kafka.clients.producer.ProducerConfig.*;
+import static java.util.UUID.randomUUID;
+import static org.apache.kafka.clients.producer.ProducerConfig.BOOTSTRAP_SERVERS_CONFIG;
+import static org.apache.kafka.clients.producer.ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG;
+import static org.apache.kafka.clients.producer.ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG;
 
 public class Producer {
-    private static Properties kafkaProperties() {
-        Properties properties = new Properties();
+	private static final Logger log = LoggerFactory.getLogger(Consumer.class);
 
-        properties.setProperty(BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        properties.setProperty(KEY_SERIALIZER_CLASS_CONFIG, Integer.class.getName());
-        properties.setProperty(VALUE_SERIALIZER_CLASS_CONFIG, Integer.class.getName());
+	private static Properties kafkaProperties() {
+		Properties properties = new Properties();
 
-        return properties;
-    }
+		properties.setProperty(BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+		properties.setProperty(KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+		properties.setProperty(VALUE_SERIALIZER_CLASS_CONFIG, IntegerSerializer.class.getName());
 
-    public static void main(String[] args) {
-        KafkaProducer<Integer, Integer> kafkaProducer = new KafkaProducer<>(kafkaProperties());
+		return properties;
+	}
 
-        range(0, 100)
-            .boxed()
-            .map(i -> new ProducerRecord<>("test", i, i))
-            .forEach(kafkaProducer::send);
+	public static void main(String[] args) {
+		log.info("Starting producer...");
 
-        kafkaProducer.close();
-    }
+		try (KafkaProducer<String, Integer> kafkaProducer = new KafkaProducer<>(kafkaProperties())) {
+			new Random()
+				.ints(100, 0, 1000000)
+				.boxed()
+				.map(i -> new ProducerRecord<>("workshop", randomUUID().toString(), i))
+				.forEach(r -> {
+					log.info("key: {}, value: {}", r.key(), r.value());
+
+					kafkaProducer.send(r);
+				});
+		}
+	}
 }
